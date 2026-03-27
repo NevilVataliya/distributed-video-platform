2140a8e82073a0b5e1757f56718ef0c97c7f5a57
Vasu9914 <u23cs106@coed.svnit.ac.in>
2026-03-27 22:50:50 +0530

    add thumbnail and duration in webhook update

--- worker-node/src/utils/sendWebhook.js
+++ worker-node/src/utils/sendWebhook.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
-export const sendWebhook = async (videoId) => {
+export const sendWebhook = async (videoId, thumbnailUrl, duration) => {
     const hlsUrl = `http://localhost:9000/processed-videos/${videoId}/playlist.m3u8`;
     try {
         const response =
             await axios.post("http://host.docker.internal:3000/api/videos/webhook", {
                 videoId,
                 status: "Ready",
-                hlsUrl
+                hlsUrl,
+                thumbnailUrl,
+                duration
             });
         console.log("Webhook sent successfully:", response.data);
     } catch (error) {
