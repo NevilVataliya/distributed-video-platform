597cabcfa716a4c853cd8d2ee4224880ea1d3b28
Nevil <nevilvataliya84@gmail.com>
2026-03-28 00:13:25 +0530

    fix: update webhook endpoint for video processing status. and made worker-node docker containeer for os agnostic

--- worker-node/src/services/chunkVideo.js
+++ worker-node/src/services/chunkVideo.js
@@ -93,7 +93,7 @@ const chunkVideo = (inputPath, outputDir, videoId) => {
                 resolve();
             } else {
                 // reject(new Error(`FFmpeg exited with code ${code}`));
-                await axios.post("http://host.docker.internal:3000/api/videos/webhook", {
+                await axios.post("http://host.docker.internal:3000/api/webhooks/processing-done", {
                     videoId,
                     status: "Failed",
                     hlsUrl: null
