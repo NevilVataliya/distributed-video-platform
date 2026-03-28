09a357035def133e8db8127de84777345c0c2643
Vasu9914 <u23cs106@coed.svnit.ac.in>
2026-03-28 17:20:52 +0530

    thumbnail upload and extracted the video duration

--- worker-node/src/utils/sendWebhook.js
+++ worker-node/src/utils/sendWebhook.js
@@ -1,17 +1,21 @@
 import axios from "axios";
 import { BUCKETS, MINIO, WEBHOOK, VIDEO_PROCESSING } from "../constants.js";
 
-export const sendWebhook = async (videoId, thumbnailUrl, duration) => {
-    const hlsUrl = `${MINIO.PUBLIC_BASE_URL}/${BUCKETS.PROCESSED_VIDEOS}/${videoId}/${VIDEO_PROCESSING.PLAYLIST_FILE_NAME}`;
+export const sendWebhook = async (videoId, duration, thumbnailUrl) => {
+    const hlsUrl = `${BUCKETS.PROCESSED_VIDEOS}/${videoId}/${VIDEO_PROCESSING.PLAYLIST_FILE_NAME}`;
+    const payload = {
+        videoId,
+        status: WEBHOOK.STATUS_READY,
+        hlsUrl,
+        duration,
+    };
+
+    if (thumbnailUrl) {
+        payload.thumbnailUrl = thumbnailUrl;
+    }
+
     try {
-        const response =
-            await axios.post(`${WEBHOOK.BASE_URL}${WEBHOOK.PROCESSING_DONE_PATH}`, {
-                videoId,
-                status: WEBHOOK.STATUS_READY,
-                hlsUrl,
-                thumbnailUrl,
-                duration
-            });
+        const response = await axios.post(`${WEBHOOK.BASE_URL}${WEBHOOK.PROCESSING_DONE_PATH}`, payload);
         console.log("Webhook sent successfully:", response.data);
     } catch (error) {
         console.error("Error sending webhook:", error);
